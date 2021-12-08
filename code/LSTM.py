a142d178dbabde63e9301922b251485c70fad56a
David Andreas Bordvik <35432806+AndreasBordvik@users.noreply.github.com>
2021-12-08 15:40:19 +0100

    working on LSTM stuff

--- code/LSTM.py
+++ code/LSTM.py
@@ -9,19 +9,17 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM, BatchNormalization, TimeDistributed
 from tensorflow.keras.callbacks import Callback, EarlyStopping, ReduceLROnPlateau, History
 from common import *
-from cmcrameri import cm 
+from cmcrameri import cm
 import sys
 from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler
 import datetime
 import os
 
-<<<<<<< HEAD
-=======
 print(os.listdir())
 
 #dataset_df = pd.read_csv(f"entsoe_dataset_main.csv", index_col=[0])
-dataset_df = pd.read_csv(f"data/input_data/entsoe_dataset_main.csv", index_col=[0])
->>>>>>> 12425874326e2625dcd885eb48aa3eb56921674d
+dataset_df = pd.read_csv(
+    f"data/input_data/entsoe_dataset_main.csv", index_col=[0])
 # dataset_df = pd.read_csv(f"{INPUT_DATA}entsoe_dataset_main.csv", index_col=[0])
 
 #dataset_df["year"] = dataset_df["dato_id"].year
@@ -40,33 +38,35 @@ dataset_has_nan = dataset_df.isna().sum().sum()
 print(dataset_df.isna().sum().sum())
 #print(dataset_df.isna().sum())
 """
-dataset_df_preprocessed = dataset_df.interpolate(method="linear", 
-                                        limit_direction="backward", 
-                                        limit=24)
+dataset_df_preprocessed = dataset_df.interpolate(method="linear",
+                                                 limit_direction="backward",
+                                                 limit=24)
 
 
 # dataset_df_preprocessed = dataset_df_preprocessed[dataset_df_preprocessed["year"] < 2021]
 
 
-#print(dataset_df_preprocessed.isna().sum())
+# print(dataset_df_preprocessed.isna().sum())
 
 #indices = pd.isnull(dataset_df["NO1_Actual Load"]).any().nonzero()[0]
 #indices = dataset_df.loc[pd.isna(dataset_df["NO1_Actual Load"]), :].index
 
 
-test, train = train_test_split(dataset_df_preprocessed, test_size=0.8, shuffle=False)
+test, train = train_test_split(
+    dataset_df_preprocessed, test_size=0.8, shuffle=False)
 #train, test = train_test_split(dataset_df_preprocessed, test_size=0.2, shuffle=False)
 train, val = train_test_split(train, test_size=0.15, shuffle=False)
 
 # Data columns
-target_colums = [f"NO{i}_day_ahead_prices" for i in range(1,6)][4]
-load_columns = [f"NO{i}_Actual Load" for i in range(1,6)][4]
-generated_columns = [f"NO{i}_generation_actual" for i in range(1,6)][4]
-load_forecast_columns = [f"NO{i}_Forecasted Load" for i in range(1,6)][4]
-generated_forecast_columns = [f"NO{i}_generation_forecast" for i in range(1,6)][4]
+target_colums = [f"NO{i}_day_ahead_prices" for i in range(1, 6)][4]
+load_columns = [f"NO{i}_Actual Load" for i in range(1, 6)][4]
+generated_columns = [f"NO{i}_generation_actual" for i in range(1, 6)][4]
+load_forecast_columns = [f"NO{i}_Forecasted Load" for i in range(1, 6)][4]
+generated_forecast_columns = [
+    f"NO{i}_generation_forecast" for i in range(1, 6)][4]
 
-feature_columns =  [generated_forecast_columns] + [load_forecast_columns] 
-#feature_columns =  generated_columns + load_columns 
+feature_columns = [generated_forecast_columns] + [load_forecast_columns]
+#feature_columns =  generated_columns + load_columns
 
 
 # Create correlation matrix
@@ -96,7 +96,7 @@ t_test = test[target_colums].to_numpy()
 X_test = test[feature_columns].to_numpy()
 
 
-# Scaling 
+# Scaling
 """"""
 print(f"{X_train.shape=}")
 print(f"{t_train.shape=}")
@@ -107,26 +107,26 @@ X_test = X_scaler.transform(X_test)
 X_val = X_scaler.transform(X_val)
 
 t_scaler = MinMaxScaler()
-t_scaler.fit(t_train.reshape(-1,1))
-t_train = t_scaler.transform(t_train.reshape(-1,1)).flatten()
-t_test = t_scaler.transform(t_test.reshape(-1,1)).flatten()
-t_val = t_scaler.transform(t_val.reshape(-1,1)).flatten()
+t_scaler.fit(t_train.reshape(-1, 1))
+t_train = t_scaler.transform(t_train.reshape(-1, 1)).flatten()
+t_test = t_scaler.transform(t_test.reshape(-1, 1)).flatten()
+t_val = t_scaler.transform(t_val.reshape(-1, 1)).flatten()
 
 
 # Model architecture
 class LSTM_model(Sequential):
     def __init__(self, eta, rnn_units, model_shape, nbf_outputs=1):
         super(self.__class__, self).__init__()
-       
+
         # LSTM layer
-        self.add(LSTM(rnn_units, input_shape = model_shape, return_sequences = True))
+        self.add(LSTM(rnn_units, input_shape=model_shape, return_sequences=True))
         #self.add(LSTM(rnn_units, input_shape = model_shape))
-        
+
         # # Dense layer
         # self.add(Dense(rnn_units, activation='relu'))
-        
+
         # Normalization
-        #self.add(BatchNormalization())
+        # self.add(BatchNormalization())
 
         # Dropouts
         self.add(Dropout(0.15))
@@ -136,15 +136,16 @@ class LSTM_model(Sequential):
 
         # Model compile settings
         opt = tf.keras.optimizers.Adam(learning_rate=eta)
-        
-        ## Compile model
+
+        # Compile model
         self.compile(loss='mse', optimizer=opt)
 
-# Model shape 
+
+# Model shape
 time_steps = 50
 nbf_features = X_train.shape[1]
 print(nbf_features)
-model_shape = (time_steps, nbf_features) 
+model_shape = (time_steps, nbf_features)
 
 # (batch_size, time_steps, nbf_features)
 # shape going into the model (488,72,10)
@@ -154,8 +155,8 @@ model_shape = (time_steps, nbf_features)
 
 # Data preprocessing
 # Training data
-X_train_m = X_train[35169-35000: ].copy() #
-t_train_m = t_train[35169-35000:].copy() # 
+X_train_m = X_train[35169-35000:].copy()
+t_train_m = t_train[35169-35000:].copy()
 
 batch_size_train = X_train_m.shape[0] // time_steps
 X_train_m = X_train_m.reshape((batch_size_train, time_steps, nbf_features))
@@ -164,16 +165,16 @@ t_train_m = t_train_m.reshape((batch_size_train, time_steps, 1))
 
 
 # Validation data
-X_val_m = X_val[6207-5000: ].copy() #
-t_val_m = t_val[6207-5000:].copy() # 
+X_val_m = X_val[6207-5000:].copy()
+t_val_m = t_val[6207-5000:].copy()
 
 batch_size_val = X_val_m.shape[0] // time_steps
 X_val_m = X_val_m.reshape((batch_size_val, time_steps, nbf_features))
 t_val_m = t_val_m.reshape((batch_size_val, time_steps, 1))
 
 # Test data
-X_test_m = X_test[10344-10000: ].copy() #
-t_test_m = t_test[10344-10000: ].copy() # 
+X_test_m = X_test[10344-10000:].copy()
+t_test_m = t_test[10344-10000:].copy()
 
 batch_size_test = X_test_m.shape[0] // time_steps
 X_test_m = X_test_m.reshape((batch_size_test, time_steps, nbf_features))
@@ -184,31 +185,31 @@ t_test_m = t_test_m.reshape((batch_size_test, time_steps, 1))
 eta = 0.01
 rnn_units = 64
 batch_size = 200
-EPOCHS=20
+EPOCHS = 20
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5,
                               patience=5, min_lr=0.0001)
 
 # Tensorboard
-log_dir = f"data/report_data/logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+log_dir = f"data/report_data/logs/fit/" + \
+    datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 # log_dir = f"{REPORT_DATA}logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+tensorboard_callback = tf.keras.callbacks.TensorBoard(
+    log_dir=log_dir, histogram_freq=1)
 
 print(t_train_m.max())
 print(t_val_m.max())
 print(t_test_m.max())
 
 
-
-model = LSTM_model(eta, rnn_units,model_shape, nbf_outputs=1)
-model.fit(X_train_m, t_train_m, 
-          shuffle=False, 
-          batch_size=batch_size, 
-          epochs=EPOCHS, 
+model = LSTM_model(eta, rnn_units, model_shape, nbf_outputs=1)
+model.fit(X_train_m, t_train_m,
+          shuffle=False,
+          batch_size=batch_size,
+          epochs=EPOCHS,
           validation_data=(X_val_m, t_val_m),
           callbacks=[reduce_lr, tensorboard_callback],
           verbose=1)
 print(model.summary())
-
 
 
 # predict
@@ -221,7 +222,6 @@ print(test_MSE)
 
 
 results = model.evaluate(X_test_m, t_test_m, verbose=0)
-
 
 
 x_plot = np.arange(t_test_m.flatten().shape[0])

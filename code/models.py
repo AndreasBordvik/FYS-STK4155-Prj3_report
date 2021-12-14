b5b8b88ba9ba6c1483a11a4884fdcbb39b154af1
David Andreas Bordvik <35432806+AndreasBordvik@users.noreply.github.com>
2021-12-14 14:07:21 +0100

    f

---
+++ code/models.py
@@ -0,0 +1,19 @@
+from tensorflow import keras
+
+class LNSimpleRNNCell(keras.layers.Layer):
+    def __init__(self, units, activation="tanh", **kwargs):
+        super().__init__(**kwargs)
+        self.state_size = units
+        self.output_size = units
+        self.simple_rnn_cell = keras.layers.SimpleRNNCell(units, activation=None)
+        self.layer_norm = keras.layers.LayerNormalization()
+        self.activation = keras.activations.get(activation)
+
+    def call(self, inputs, states):
+        outputs, new_states = self.simple_rnn_cell(inputs, states)
+        norm_outputs = self.activation(self.layer_norm(outputs))
+        return norm_outputs, [norm_outputs]
+
+
+if __name__ == '__main__':
+    print("Import this file as a package please!")
